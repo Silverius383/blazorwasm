5ad5f7db4e176493507024c64913102c37262643
Silverius383 <silverius.stanic@si.ukdw.ac.id>
2022-11-23 18:28:41 +0700

    Pertemuan 11 : Mudblazor component

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazorwasm;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7014/") });
 
